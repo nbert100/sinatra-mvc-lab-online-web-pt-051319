1377ea8ad5a94bacfba136ccce4e9d9b420e69c5
Nia Bert <nbert14@gmail.com>
2019-08-21 02:02:09 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,29 @@
+
+class PigLatinizer
+  attr_accessor :phrase
+  
+  def initialize
+    @phrase = phrase
+  end
+  
+  def piglatinize_word(word)
+    word.split("")
+    vowel = [Aa, Ee, Ii, Oo, Uu]
+    case first_letter
+    when first_letter == vowel
+      
+    when first_letter == consonant
+      
+    
+  end
+  # noodle soup --> noodle + soup
+  
+  # split " "
+  
+  # noodle --> oodlenay
+  # array.each do unshift 
+  #   unless vowel
+    
+  
+  
+end 
